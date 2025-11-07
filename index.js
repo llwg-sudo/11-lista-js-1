0ba8e362ef5209cca7e74a162e9ae7482db99c5c
llwg-sudo <llwg@cesar.school>
2025-11-07 17:15:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 /**** Escreva o código abaixo 👇******/
 
+//Questão 1: Soma simples
+function somar(a,b) {
+  return a + b;
+}
 
+//Questão 2: Multiplicação simples
+function multiplicar(c, d) {
+  return c * d;
+}
 
+//Questão 3: Subtração simples
+function subtrair(e, f) {
+  return e - f;
+}
 
+//Questão 7: Área de um retângulo
+function calcularAreaRetangulo(largura, altura) {
+  area = multiplicar(largura, altura);
+  return area;
+}
 
-
-
+//Questão 8: Par ou ímpar
+function verificarParOuImpar(numero) {
+  if (numero % 2 === 0) {
+    return "par";
+  } else {
+    return "ímpar";
+  }
+}
 
 /**** Escreva o código acima ☝️******/
 
